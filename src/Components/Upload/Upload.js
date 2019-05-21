40d92b5d5088d4e68911dcd58edbe5ddb1f163d6
Artix1500 <artur.haczek23@gmail.com>
2019-05-21 11:24:45 +0200

    Send request with file

--- src/Components/Upload/Upload.js
+++ src/Components/Upload/Upload.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import './Upload.scss'
-import Dropzone from '../Dropzone/Dropzone'
+import axios from 'axios';
 
 class Upload extends Component {
   constructor(props) {
     super(props)
   }
+
+    handleUploadFile = async (event) => {
+        const data = new FormData();
+        data.append('file', event.target.files[0]);
+        console.log(data);
+        // '/files' is your node.js route that triggers our middleware
+        const text = await axios.get('http://127.0.0.1:5000/api', data, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+        });
+        console.log(text)
+    }
+        
 
   render() {
     return (
@@ -16,8 +30,7 @@ class Upload extends Component {
                 <input
                 className="File-Input"
                 type="file"
-                multiple
-                onChange={this.onFilesAdded}
+                onChange={this.handleUploadFile}
                 />
             </div>
         </div>
