e1fcbff225d41b312109a62d69d7c33f2760c07d
Artix1500 <artur.haczek23@gmail.com>
2019-05-21 08:58:11 +0200

    Initialize react application

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          un-sdgs-content-linking
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
