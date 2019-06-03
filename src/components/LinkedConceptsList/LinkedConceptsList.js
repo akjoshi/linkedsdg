14c04ffa0f245d463743230cadcc726502a19c5a
Artix1500 <artur.haczek23@gmail.com>
2019-06-03 09:44:33 +0200

    Add option to load 10 previous records

--- src/components/LinkedConceptsList/LinkedConceptsList.js
+++ src/components/LinkedConceptsList/LinkedConceptsList.js
@@ -19,12 +19,19 @@ class LinkedConceptsList extends React.Component {
 
     displayData = async () => {
         const { data, loadCount } = this.state;
-        const chunkOfData = await data.filter((data, index) => index < loadCount)
-        this.setState({displayData: chunkOfData});
+        const chunkOfData = await data.filter((data, index) => index < loadCount && index >= loadCount - 10)
+        this.setState({ displayData: chunkOfData });
     }
 
     loadMore = async () => {
-        await this.setState({ loadCount: this.state.loadCount+10 });
+        await this.setState({ loadCount: this.state.loadCount + 10 });
+
+        this.displayData()
+    }
+
+    loadLess = async () => {
+        await this.setState({ loadCount: this.state.loadCount - 10 });
+
         this.displayData()
     }
 
@@ -35,7 +42,18 @@ class LinkedConceptsList extends React.Component {
                 <ul>
                     {this.state.displayData.map((data, index) => <ConceptItem data={data} key={index}></ConceptItem>)}
                 </ul>
-                <p onClick={this.loadMore}>Load 10 more</p>
+
+                {this.state.loadCount < this.state.data.length ? (
+                    <p onClick={this.loadMore}>Next</p>
+                ) : (
+                        <p> Next but not working</p>
+                    )}
+
+                {this.state.loadCount > 10 ? (
+                    <p onClick={this.loadLess}>Previous</p>
+                ) : (
+                        <p> Previous but not working</p>
+                    )}
             </React.Fragment>
         );
     }
