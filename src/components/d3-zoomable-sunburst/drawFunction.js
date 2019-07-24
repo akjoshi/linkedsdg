e6e4cc3faeb9da4b4babc7d46a1188e892ae15ee
Artix1500 <artur.haczek23@gmail.com>
2019-07-24 16:52:10 +0200

    Cut sunburst into few files

---
+++ src/components/d3-zoomable-sunburst/drawFunction.js
@@ -0,0 +1,275 @@
+import * as d3 from "d3";
+import axios from 'axios';
+
+export async function drawChart(){
+    const uris = require('./sdgURIS.json')
+
+    const mouseover = (p) => {
+        this.setState({
+            dataForPreview: {
+                id: p.data.id,
+                name: p.data.name,
+                label: p.data.label,
+                concept: p.data.concept
+            }
+        })
+        if (p.parent === null) {
+            this.setState({ selectedGoal: p.data.id, selectedGoalName: p.data.name })
+        }
+        else if (this.state.selectedGoal === undefined) {
+            if (uris.includes(p.data.id)) {
+                this.setState({ selectedGoal: p.data.id, selectedGoalName: p.data.name })
+            }
+            else if (p.parent !== null && uris.includes(p.parent.data.id)) {
+
+                this.setState({ selectedGoal: p.parent.data.id, selectedGoalName: p.parent.data.name })
+            }
+        }
+    }
+
+    const mouseout = (p) => {
+        this.setState({
+            dataForPreview: undefined
+        })
+        if (this.state.clickedData.id === undefined) {
+            this.setState({ selectedGoal: undefined, selectedGoalName: 'Sustainable Development Goals' })
+        }
+
+    }
+
+    const clicked = async (p) => {
+        parent.datum(p.parent || root);
+
+        this.chooseState(p)
+
+
+        root.each(d => d.target = {
+            x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            y0: Math.max(0, d.y0 - p.depth),
+            y1: Math.max(0, d.y1 - p.depth)
+        });
+
+        this.setState({
+            clickedData: {
+                id: p.data.id,
+                name: p.data.name,
+                label: p.data.label,
+                concept: p.data.concept
+            }
+        })
+        if (p.parent === null) {
+            this.setState({ selectedGoal: p.data.id, selectedGoalName: p.data.name })
+        }
+        else if (this.state.selectedGoal === undefined) {
+            if (uris.includes(p.data.id)) {
+                this.setState({ selectedGoal: p.data.id, selectedGoalName: p.data.name })
+            }
+            else if (p.parent !== null && uris.includes(p.parent.data.id)) {
+
+                this.setState({ selectedGoal: p.parent.data.id, selectedGoalName: p.parent.data.name })
+            }
+        }
+
+        const t = g.transition().duration(750);
+
+        if (p.data.label !== undefined) {
+
+            let midTitle = g.select("text#middleTitle");
+
+            let middleTitle = this.state.sunState;
+            middleTitle = middleTitle + "s";
+            if (middleTitle === 'seriess') {
+                middleTitle = "Series";
+            }
+
+            middleTitle = "BACK TO " + middleTitle.toUpperCase();
+
+            midTitle.text(function (d) { return middleTitle })
+                .attr("x", 0)
+                .attr("y", "10px")
+        }
+        else {
+            g.select("text#middleTitle")
+                .text(function (d) { return "" })
+                .attr("x", 0)
+                .attr("y", 0)
+        }
+
+        path.transition(t)
+            .tween("data", d => {
+                const i = d3.interpolate(d.current, d.target);
+                return t => d.current = i(t);
+            })
+            .filter(function (d) {
+                return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+            })
+            .attrTween("d", d => () => arc(d.current));
+
+
+        if (p.children !== undefined) {
+            path.transition(t)
+                .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 1 : 0.6) : 0)
+        }
+
+        label.filter(function (d) {
+            return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+        }).transition(t)
+            .attr("fill-opacity", d => +labelVisible(d.target))
+            .attrTween("transform", d => () => labelTransform(d.current));
+
+
+        if (this.state.clickedData.label !== undefined && this.state.clickedData.label.split(" ")[0] === "Series") {
+            try {
+                const dataForApi = {
+                    "countries": this.props.dataForSeries,
+                    "stat": this.state.clickedData.id
+                }
+
+                const text = await axios.post('http://127.0.0.1:5002/stats', dataForApi, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                
+                if (text.status !== 200 && text.status !== 201) {
+                    throw new Error('Failed!');
+                }
+                text.data['@graph'].sort(function (a, b) {
+                    let textA = a.geoAreaName.toUpperCase();
+                    let textB = b.geoAreaName.toUpperCase();
+                    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                });
+                this.setState({ countrySeriesData: text.data['@graph'] })
+
+            } catch (error) {
+                console.log("ERROR");
+            }
+        }
+        else {
+            this.setState({ countrySeriesData: [] })
+        }
+    }
+
+    let width = 932
+    let radius = 155.33333333333334
+    let arc = d3.arc()
+        .startAngle(d => d.x0)
+        .endAngle(d => d.x1)
+        .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+        .padRadius(radius * 1.5)
+        .innerRadius(d => d.y0 * radius)
+        .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
+
+
+    let data = this.props.data;
+
+    const partition = data => {
+        const root = d3.hierarchy(data)
+            .sum(d => d.value)
+            .sort((a, b) => b.value - a.value);
+        return d3.partition()
+            .size([2 * Math.PI, root.height + 1])(root);
+    }
+
+    const root = partition(data);
+
+    let colors = require(`./sdgColors.json`)
+
+    root.each(d => d.current = d);
+
+    const svg = d3.select("#ZoomableSunburst")
+        .append("svg")
+        .attr("viewBox", [0, 0, width, width])
+        .style("font", "10px sans-serif");
+
+    const g = svg.append("g")
+        .attr("transform", `translate(${width / 2},${width / 2})`);
+
+    const path = g.append("g")
+        .selectAll("path")
+        .data(root.descendants().slice(1))
+        .join("path")
+        .attr("fill", d => { while (d.depth > 1) d = d.parent; return colors[d.data.id]; })
+        .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 1 : 0.6) : 0)
+        .attr("d", d => arc(d.current));
+
+    path.filter(d => d)
+        .style("cursor", d => {
+            if (!arcVisible(d.current)) {
+                return ""
+            }
+            return "pointer"
+        })
+        .on("click", d => {
+            if (arcVisible(d.current || (d.children === undefined && arcVisible(d.current)))) {
+                clicked(d)
+                mouseout(d)
+            }
+        })
+        .on("mouseover", d => {
+            if (arcVisible(d.current) || (d.children === undefined && arcVisible(d.current))) {
+                mouseover(d)
+            }
+        })
+        .on("mouseout", d => {
+            if (arcVisible(d.current) || (d.children === undefined && arcVisible(d.current))) {
+                mouseout(d)
+            }
+        })
+
+    const label = g.append("g")
+        .attr("pointer-events", "none")
+        .attr("text-anchor", "middle")
+        .style("user-select", "none")
+        .selectAll("text")
+        .data(root.descendants().slice(1))
+        .join("text")
+        .attr("dy", "0.35em")
+        .attr("fill-opacity", d => +labelVisible(d.current))
+        .attr("transform", d => labelTransform(d.current))
+        .text(d => {
+            return d.data.label;
+        })
+        .style("font-size", (d) => {
+            if (d.data.label.split(" ")[0] !== 'Series') {
+                return "16px"
+            }
+            return "12px"
+        })
+
+
+    let parent = g.append("circle")
+        .datum(root)
+        .attr("r", radius)
+        .attr("fill", "none")
+        .attr("pointer-events", "all")
+
+    parent = g
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("id", "middleTitle")
+        .text(function (d) { return "" })
+        .style("font-size", "28px")
+        .style("font-weight", "700")
+        .style("cursor", "pointer")
+        .attr("pointer-events", "all")
+        .on("click", clicked);
+
+    function arcVisible(d) {
+        return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+    }
+
+    function labelVisible(d) {
+        return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+    }
+
+    function labelTransform(d) {
+        const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+        const y = (d.y0 + d.y1) / 2 * radius;
+        return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+    }
+
+    return svg.node();
+}
+
---
+++ src/components/d3-zoomable-sunburst/utilities.js
@@ -0,0 +1,220 @@
+import React from 'react';
+import axios from 'axios';
+import Wheel from './img/wheel.png';
+import './ZoomableSunburst.scss'
+
+export function setSunState(deeper = true) {
+    if (deeper === true) {
+        if (this.state.sunState === "root") {
+            this.setState({ sunState: "goal" })
+        }
+        else if (this.state.sunState === "goal") {
+            this.setState({ sunState: "target" })
+        }
+        else if (this.state.sunState === "target") {
+            this.setState({ sunState: "indicator" })
+        }
+        else if (this.state.sunState === "indicator") {
+            this.setState({ sunState: "series" })
+        }
+    }
+    else {
+        if (this.state.sunState === "series") {
+            this.setState({ sunState: "indicator" })
+        }
+        else if (this.state.sunState === "indicator") {
+            this.setState({ sunState: "target" })
+        }
+        else if (this.state.sunState === "target") {
+            this.setState({ sunState: "goal" })
+        }
+        else if (this.state.sunState === "goal") {
+            this.setState({ sunState: "root" })
+        }
+    }
+}
+
+export function chooseState(p){
+    if(this.state.lastNode === p.parent || this.state.lastNode === undefined){
+        this.setSunState(true)
+    }
+    else if(p.parent !== null && this.state.lastNode === p.parent.parent){
+        this.setSunState(true)
+        this.setState({lastNode: p})
+        this.setSunState(true)
+    }
+    else{
+        this.setSunState(false)
+    }
+    this.setState({lastNode: p})
+}
+
+export function  selectImage(){
+    let images = {}
+    let index = 1;
+    while (index < 18) {
+        let indexNr = ""
+        index < 10 ? indexNr = "0" + index : indexNr = index;
+        images["http://data.un.org/kos/sdg/" + indexNr] = require(`./img/${indexNr}.png`);
+        index = index + 1;
+    }
+
+    if (this.state.selectedGoal === undefined) {
+        return <img className="goal-image" src={Wheel} alt="Goal img"></img>;;
+    }
+    return <img className="goal-image" src={images[this.state.selectedGoal]} alt="Goal img"></img>;
+}
+
+export function  loadConcepts(){
+    let data = [];
+
+    for (var url in this.state.clickedData.concept) {
+        let linkedConcepts = [];
+
+        for (var url2 in this.state.clickedData.concept[url]['subconcepts']) {
+            linkedConcepts.push({
+                url: url2,
+                label: this.state.clickedData.concept[url]['subconcepts'][url2].label,
+            })
+        }
+
+        data.push({
+            url: url,
+            label: this.state.clickedData.concept[url].label,
+            source: this.state.clickedData.concept[url].source,
+            linkedConcepts: linkedConcepts
+        })
+    }
+
+    return data.map(x => {
+        let linkedConceptsSorted = []
+        for (let k in x.linkedConcepts) {
+            if (x.linkedConcepts[k].label === x.label) {
+                linkedConceptsSorted.push(x.linkedConcepts[k])
+            }
+        }
+        for (let k in x.linkedConcepts) {
+            if (x.linkedConcepts[k].label !== x.label) {
+                linkedConceptsSorted.push(x.linkedConcepts[k])
+            }
+        }
+        return <p key={x.url}>
+            <span>{x.label}</span> concept from {x.source}:
+            {linkedConceptsSorted.map(y => {
+                if (y.label !== x.label) {
+                    return <span key={y.url} className="uri-link">
+                        <br></br>
+                        <a href={y.url}> {y.label} +</a>
+                    </span>
+                }
+                return <i key={y.url} className="uri-link">
+                    <br></br>
+                    <a href={y.url}> {y.label}</a>
+                </i>
+            })}
+        </p>
+    })
+}
+
+export function  callForCountryApi(x){
+    return async () => {
+        try {
+            const dataForApi = {
+                "type": "Country",
+                "uri": x['@id']
+            }
+
+            const text = await axios.post('http://34.66.148.181:8080/describe', dataForApi, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (text.status !== 200 && text.status !== 201) {
+                throw new Error('Failed!');
+            }
+
+            var myWindow = window.open("", "MsgWindow");
+            myWindow.document.write('<pre id="json"></pre>');
+            myWindow.document.getElementById("json").innerHTML = JSON.stringify(text.data, undefined, 2);
+
+        } catch (error) {
+            console.log("ERROR");
+        }
+    }
+}
+
+export function  genOnClick(x){
+    return () => {
+        let elem = document.getElementById("Series" + x['@id'])
+        if (elem.style.height === "auto") {
+            elem.style.height = "24px"
+        }
+        else {
+            elem.style.height = "auto";
+        }
+    }
+}
+
+export function  getJsonText(x){
+    if (typeof (x) === 'object') {
+        let tab = require(`./jsonSeriesData.json`);
+        let data = []
+        for (let y in x) {
+            if (tab.filter(x => x === y).length > 0) {
+                data.push(y + ": " + x[y]);
+            }
+        }
+        return data.map(elem => { return <li key={x['@id'] + elem.split(" ")[1]}>{elem}</li> })
+    }
+}
+
+export async function  reciveSeriesJsonFromApi(){
+    try {
+        const dataForApi = {
+            "countries": this.props.dataForSeries,
+            "stat": this.state.clickedData.id
+        }
+
+        const text = await axios.post('http://127.0.0.1:5002/stats', dataForApi, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (text.status !== 200 && text.status !== 201) {
+            throw new Error('Failed!');
+        }
+        
+        var myWindow = window.open("", "MsgWindow");
+        myWindow.document.write('<pre id="json"></pre>');
+        myWindow.document.getElementById("json").innerHTML = JSON.stringify(text.data, undefined, 2);
+
+    } catch (error) {
+        console.log("ERROR");
+    }
+}
+
+export async function  reciveJsonFromApi(){
+    try {
+
+        const dataForApi = {
+            "type": this.state.clickedData.label.split(" ")[0],
+            "uri": this.state.clickedData.id
+        }
+
+        const text = await axios.post('http://34.66.148.181:8080/describe', dataForApi, {
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        });
+        if (text.status !== 200 && text.status !== 201) {
+            throw new Error('Failed!');
+        }
+        
+        var myWindow = window.open("", "MsgWindow");
+        myWindow.document.write('<pre id="json"></pre>');
+        myWindow.document.getElementById("json").innerHTML = JSON.stringify(text.data, undefined, 2);
+
+    } catch (error) {
+        console.log("ERROR");
+    }
+}
