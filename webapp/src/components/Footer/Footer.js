94403158488de6c7c087f9e6b3e94fe0546db066
Amit Joshi <joshi6@un.org>
2019-11-19 17:00:51 +0000

    Update Footer.js  - use UN DESA logo and link.
    Former-commit-id: 6b0baa98c2b17948018d9556c999028c8ee99ef5

--- webapp/src/components/Footer/Footer.js
+++ webapp/src/components/Footer/Footer.js
@@ -8,9 +8,9 @@ const Footer = props => (
     <footer className="main-footer">
         <div className="logo-container">
             <p className="describe">Delivered by</p>
-            <a href=" http://epistemik.co">
-                <img src={logo} alt="Epistemik"></img>
-            </a>
+              <a href="https://www.un.org/development/desa/">
+                <img src={logo} alt="UN DESA"></img>
+                </a>
             <a href="http://www.sdg.org/">
                 <img src={sdgLogo} alt="SDG logo"></img>
             </a>
