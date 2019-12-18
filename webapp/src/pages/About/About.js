76744bea0f9092c0162d2c2064f516c1a4725e63
Artix1500 <artur.haczek23@gmail.com>
2019-12-18 11:02:05 +0100

    Add Contributors to about page
    Former-commit-id: 5c57e0976104318b01f150ea65e67ebae52425e2

--- webapp/src/pages/About/About.js
+++ webapp/src/pages/About/About.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Logo from './LinkedSDG_square.jpg';
+import logoUN from './logos/logo.png';
+import logoEpis from './logos/logoEpistemik.png';
+import logoSDG from './logos/sdglogo.jpg';
+import logoUNG from './logos/ung-sq.png';
 
 import './About.scss'
 
@@ -23,34 +27,54 @@ class About extends Component {
                         <Row>
                             <Col lg={12}>
                                 <Row>
-                                    <Col lg={8}>
+                                    <Col lg={8} >
 
                                         <h3 className="title">About</h3>
-                                        <p className="Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique libero vitae pellentesque finibus. Nullam enim tortor, lobortis id turpis id, posuere vehicula purus. Nam gravida commodo libero, sed iaculis justo tristique aliquet. Aliquam ut lobortis justo. Sed sed sodales sapien, vel porttitor arcu. In aliquet ut tellus a pellentesque. Etiam varius lorem eu dolor elementum, eu pellentesque diam dictum. Morbi vulputate mauris non aliquet vehicula. Donec eget ligula eu libero tempus faucibus. Phasellus porttitor, enim id scelerisque rutrum, leo nunc blandit justo, sed interdum eros augue eget felis. Cras posuere, ipsum sit amet eleifend maximus, augue urna finibus augue. </p>
-
+                                        <p className="Description">
+                                            LinkedSDGs application has been developed under the leadership of the Statistics Division and the Division for Sustainable Development Goals’ (DSDG) of the Department of Economic and Social Affairs, with the support of resources from the EU grant entitled “SD2015: delivering on the promise of the SDGs”. It showcases the usefulness of adopting Semantic Web technologies and Linked Open Data principles for extracting SDG related metadata from documents and establishing the connections among various SDGs.  It is hosted under the infrastructure of the UN Global Platform for Official Statistics initiative.  For more information, see:
+                                            <a href="https://sustainabledevelopment.un.org/LinkedSDGs/about" target="_blank" className="about-link">
+                                                <span >https://sustainabledevelopment.un.org/LinkedSDGs/about</span>
+                                            </a> .
+                                        </p>
                                     </Col>
                                     <Col lg={4} className="main-image">
-
                                         <img src={Logo} alt="Upload"></img>
                                     </Col>
                                 </Row>
-                                
+
                             </Col>
                         </Row>
-
                         <Row>
                             <Col lg={12}>
                                 <h3 className="title">Contributors</h3>
-                                <p className="Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique libero vitae pellentesque finibus. Nullam enim tortor, lobortis id turpis id, posuere vehicula purus. Nam gravida commodo libero, sed iaculis justo tristique aliquet. Aliquam ut lobortis justo. Sed sed sodales sapien, vel porttitor arcu. In aliquet ut tellus a pellentesque. Etiam varius lorem eu dolor elementum, eu pellentesque diam dictum. Morbi vulputate mauris non aliquet vehicula. Donec eget ligula eu libero tempus faucibus. Phasellus porttitor, enim id scelerisque rutrum, leo nunc blandit justo, sed interdum eros augue eget felis. Cras posuere, ipsum sit amet eleifend maximus, augue urna finibus augue. </p>
-
                             </Col>
+                        </Row>
+                        <Row className="logos">
+
+                            <Col lg={3} md={6} xs={12} >
+                                <img src={logoUN} alt="logoUN"></img>
+                            </Col>
+                            <Col lg={3} md={6} xs={12} >
+                                <img src={logoEpis} alt="logoEpis"></img>
+                            </Col>
+                            <Col lg={3} md={6} xs={12} >
+                                <img src={logoSDG} alt="logoSDG"></img>
+                            </Col>
+                            <Col lg={3} md={6} xs={12} >
+                                <img src={logoUNG} alt="logoUNG"></img>
+                            </Col>
+
                         </Row>
 
                         <Row>
                             <Col lg={12}>
                                 <h3 className="title">Contact</h3>
-                                <p className="Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique libero vitae pellentesque finibus. Nullam enim tortor, lobortis id turpis id, posuere vehicula purus. Nam gravida commodo libero, sed iaculis justo tristique aliquet. Aliquam ut lobortis justo. Sed sed sodales sapien, vel porttitor arcu. In aliquet ut tellus a pellentesque. Etiam varius lorem eu dolor elementum, eu pellentesque diam dictum. Morbi vulputate mauris non aliquet vehicula. Donec eget ligula eu libero tempus faucibus. Phasellus porttitor, enim id scelerisque rutrum, leo nunc blandit justo, sed interdum eros augue eget felis. Cras posuere, ipsum sit amet eleifend maximus, augue urna finibus augue. </p>
-
+                                <p className="Description">
+                                    If you have any questions, contact us!
+                                </p>
+                                <p className="Description">
+                                    Email: .....@.....
+                                </p>
                             </Col>
                         </Row>
                     </div>
