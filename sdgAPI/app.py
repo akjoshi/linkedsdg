b78311dee7cf640e337119deb10c27057983e283
Artix1500 <artur.haczek23@gmail.com>
2019-12-20 16:19:46 +0100

    Bug in sdgAPI file path
    Former-commit-id: 81645903d5b3b77af28f72be14f193b322465f44

--- sdgAPI/app.py
+++ sdgAPI/app.py
@@ -18,38 +18,62 @@ CORS(app)
 
 @app.route('/file', methods=['POST'])
 def get_task_file():
+    response_obj = {} 
     if 'file' not in request.files:
-        abort(400)
-    file = request.files['file']
+        abort(400) 
+    
+    print(request.files["file"].filename)
 
-    abort(400)
-    return 'Something went wrong, try again!'
+    # TEXT EXTRACT
+    url1 = 'http://linkedsdg.apps.officialstatistics.org/text/api' 
+    headers = {'Content-type': 'multipart/form-data'} 
+
+    r1 = requests.request("POST", url1, files={'file':request.files['file']}, headers=headers) 
+
+    print(r1.text)
+    abort(400) 
+    response_obj["text"] = r1.json()
+    print("LELELELELE\n\n\n\n\n\n\n\n\n")
+
+    url2 = 'http://linkedsdg.apps.officialstatistics.org/concepts/api'
+    payload2 = json.dumps(response_obj["text"])
+    headers2 = {'Content-type': 'application/json'} 
+    r2 = requests.request("POST", url2, data=payload2, headers=headers2)
+    
+    response_obj["concepts"] = r2.json()
+
+    url3 = "http://linkedsdg.apps.officialstatistics.org/graph/api"
+    payload3 = json.dumps(response_obj["concepts"]['concepts'])
+    r3 = requests.request("POST", url3, data=payload3, headers=headers2)
+    
+    response_obj["graph"] = r3.json()
+
+    return json.dumps(response_obj)
 
 
 @app.route('/url', methods=['POST'])
 def get_task_url():
-    response_obj = {}
-    print(request.data)  
+    response_obj = {}  
 
     # TEXT EXTRACT
     url = 'http://linkedsdg.apps.officialstatistics.org/text/apiURL'
     payload = str(request.data, "utf-8")
-  
     r1 = requests.request("POST", url, data=payload)
-    # r = requests.post(url, data=payload) 
  
-
-    response_obj["textAPI"] = r1.json()
-
+    response_obj["text"] = r1.json()
 
     url2 = 'http://linkedsdg.apps.officialstatistics.org/concepts/api'
-    payload2 = json.dumps(response_obj["textAPI"])
+    payload2 = json.dumps(response_obj["text"])
     headers2 = {'Content-type': 'application/json'} 
     r2 = requests.request("POST", url2, data=payload2, headers=headers2)
     
+    response_obj["concepts"] = r2.json()
+
+    url3 = "http://linkedsdg.apps.officialstatistics.org/graph/api"
+    payload3 = json.dumps(response_obj["concepts"]['concepts'])
+    r3 = requests.request("POST", url3, data=payload3, headers=headers2)
     
-    response_obj["contextExtractedAPI"] = r2.json()
- 
+    response_obj["graph"] = r3.json()
 
     return json.dumps(response_obj)
 
