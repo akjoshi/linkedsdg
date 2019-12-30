ddb097e047a1047ff1a34bb8526942a88cf8ed7d
Artix1500 <artur.haczek23@gmail.com>
2019-12-30 15:07:32 +0100

    refactor
    Former-commit-id: 69da1fe0b7193f6ad59ff2861b37a3095a4da3cf

--- sdgAPI/app.py
+++ sdgAPI/app.py
@@ -19,8 +19,7 @@ app.config['UPLOAD_FOLDER'] = './'
 CORS(app)
 
 @app.route('/file', methods=['POST'])
-def get_task_file():
-    response_obj = {} 
+def get_task_file(): 
     if 'file' not in request.files:
         abort(400) 
      
@@ -30,33 +29,23 @@ def get_task_file():
     files = {'file': (f.filename, f.stream, f.content_type, f.headers)}
     r1 = requests.request("POST", url1, files=files)  
      
-    response_obj["text"] = r1.json() 
-
-    url2 = 'http://linkedsdg.apps.officialstatistics.org/concepts/api'
-    payload2 = json.dumps(response_obj["text"])
-    headers2 = {'Content-type': 'application/json'} 
-    r2 = requests.request("POST", url2, data=payload2, headers=headers2)
-    
-    response_obj["concepts"] = r2.json()
-
-    url3 = "http://linkedsdg.apps.officialstatistics.org/graph/api"
-    payload3 = json.dumps(response_obj["concepts"]['concepts'])
-    r3 = requests.request("POST", url3, data=payload3, headers=headers2)
-    
-    response_obj["graph"] = r3.json()
-
-    return json.dumps(response_obj)
+    return create_response(r1)
 
 
 @app.route('/url', methods=['POST'])
-def get_task_url():
-    response_obj = {}  
-
+def get_task_url(): 
     # TEXT EXTRACT
     url = 'http://linkedsdg.apps.officialstatistics.org/text/apiURL' 
     payload = str(request.data, "utf-8")
     r1 = requests.request("POST", url, data=payload)
  
+    return create_response(r1)
+ 
+
+
+def create_response(r1):
+
+    response_obj = {}
     response_obj["text"] = r1.json()
 
     url2 = 'http://linkedsdg.apps.officialstatistics.org/concepts/api'
@@ -80,11 +69,10 @@ def get_task_url():
     url3 = "http://linkedsdg.apps.officialstatistics.org/graph/api"
     payload3 = json.dumps(data['concepts'])
     r3 = requests.request("POST", url3, data=payload3, headers=headers2)
-    
+     
     response_obj["sdgs"] = r3.json()
 
     return json.dumps(response_obj)
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=False)
