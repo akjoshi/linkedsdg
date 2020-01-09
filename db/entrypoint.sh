a11182e0aeba6a9366898a683a6e79696efccf40
Artix1500 <artur.haczek23@gmail.com>
2020-01-09 09:36:00 +0100

    Add series data to jena
    Former-commit-id: e41504a972f1d99fbcfac1b733ee886edf1d53ba

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -8,4 +8,5 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:3030/ind
 done
 echo "Starting upload..."
 curl -F file=@/tmp/apache-jena-fuseki-3.13.1/bin/sdgs-data.nq http://localhost:3030/sdgs/data
+curl -F file=@/tmp/apache-jena-fuseki-3.13.1/bin/stats.nq http://localhost:3030/stats/data
 echo "I succeed!!!"
