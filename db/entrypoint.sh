e4a7da57226494f1adbe0358a4ec3205c845dfbb
Artix1500 <artur.haczek23@gmail.com>
2020-01-10 11:31:01 +0100

    Add endpoint to swagger
    Former-commit-id: cb46fee9814e07d2e401471d3fa7dfa1999ae89f

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -10,3 +10,4 @@ echo "Starting upload..."
 curl -F file=@/tmp/apache-jena-fuseki-3.13.1/bin/sdgs-data.nq http://localhost:3030/sdgs/data
 curl -F file=@/tmp/apache-jena-fuseki-3.13.1/bin/stats.nq http://localhost:3030/stats/data
 echo "I succeed!!!"
+tail -f /dev/null
